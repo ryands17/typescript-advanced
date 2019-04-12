06f71df9f5fa5bece4060547b04ec45939bfa47b
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2019-04-13 00:50:17 +0530

    show typescripts number displaying support

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-console.log('data')
+import './utils/largeNumbers'
---
+++ src/utils/largeNumbers.ts
@@ -0,0 +1,12 @@
+class Amount {
+  constructor(private amount: number) {}
+
+  getAmount() {
+    return this.amount
+  }
+}
+
+// typescript allows cleaner depiction of numbers
+const moneyIWishIHad = new Amount(1_000_000_00)
+
+console.log('amount', moneyIWishIHad.getAmount())
