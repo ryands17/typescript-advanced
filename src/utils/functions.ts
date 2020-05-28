9dab036f54ef6376fcade2e65f26295ad49caa84
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-05-28 19:05:32 +0530

    get typing for variables and return type

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,1 +1,18 @@
 export const prettyPrint = (json: any) => JSON.stringify(json, null, 2)
+
+export const sum = (a: number, b: number) => {
+  console.log(a, b)
+  return a + b
+}
+
+export const hoc = <T extends any[], U extends any>(fn: (...args: T) => U) => {
+  console.log('mapping function')
+  return (...args: T) => {
+    return fn(...args)
+  }
+}
+
+// this has the same types as the `sum` function!
+const anotherFn = hoc(sum)
+
+console.log('called: ', anotherFn(1, 5))
