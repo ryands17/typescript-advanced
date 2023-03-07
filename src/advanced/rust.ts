b67605470dd9a9689dbe5fcfe9b0363018dce210
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-03-07 16:07:25 +0100

    update result value

--- src/advanced/rust.ts
+++ src/advanced/rust.ts
@@ -38,18 +38,19 @@ const getUserAsync = async (id: number): Promise<Maybe<User>> => {
   return { id, name: 'John' };
 };
 
-function sleep(time = 2000) {
+function sleep(time = 1000) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-export const unwrap = <T>(option: Option<T>, errorMessage?: string): T => {
-  if (option.type === 'none') throw Error(errorMessage || 'Cannot unwrap None');
-  return option.value;
+export const unwrap = <T>(rawValue: Option<T>, errorMessage?: string): T => {
+  if (rawValue.type === 'none')
+    throw Error(errorMessage || 'Cannot unwrap None');
+  return rawValue.value;
 };
 
-export const unwrapOr = <T>(option: Option<T>, defaultValue: T): T => {
+export const unwrapOr = <T>(rawValue: Option<T>, defaultValue: T): T => {
   try {
-    return unwrap(option);
+    return unwrap(rawValue);
   } catch (error) {
     return defaultValue;
   }
@@ -86,8 +87,8 @@ export const strictOption = <T>(value: Option<Maybe<T>>): Option<T> => {
 };
 
 const arr = [1, 2, 3];
-const val = createOption(arr.pop());
-console.log(unwrapOr(val, 1));
+const val = createOption(arr.find((x) => x === 4));
+console.log('Find value:', unwrapOr(val, 1));
 
 const user1 = unwrap(getValue(() => getUser(1)));
 const user2 = unwrapOr(
@@ -98,8 +99,10 @@ const user2 = unwrapOr(
 (async () => {
   const user = strictOption(await getValueAsync(getUserAsync(1)));
 
+  console.log('Using unwrap:', unwrap(user, 'Cannot find user'));
+
   if (user.type === 'some') {
-    console.log(user.value.name);
+    console.log('Using a condition:', user.value.name);
   }
 })();
 
@@ -125,5 +128,5 @@ export const getResult = <Args extends unknown[], Res>(
 
 const user = getResult(getUser)(1);
 if (user.type === 'ok') {
-  console.log(user.value.name);
+  console.log('Using the result type:', user.value.name);
 }
--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@
 // import './utils/otherImportantTypes'
 // import './utils/fp'
 // import './utils/transducers'
-import './utils/functions'
+// import './utils/functions'
+import './advanced/rust';
